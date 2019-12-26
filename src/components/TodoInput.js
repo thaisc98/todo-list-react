9f6d0ec2377c18bc19d66a6e3373e8d54edb02f3
Thais Contreras <thais_contreras@claro.com.do>
2019-12-26 15:35:42 -0400

    Todo-list finish

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,21 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class TodoInput extends Component {
-    render() {
-        return (
-            <div className="card card-body my-3">
-                <form>
-                    <div className="input-group">
-                        <div className="input-group-prepend">
-                            <div className="input-group-text bg-primary text-white">
-                                <i className="fas fa-book"></i>
-                            </div>
-                        </div>
-                        <input type="text" className="form-control" placeholder="Add to-do item"/>
-                    </div>
-                    <button type="submit"></button>
-                </form>
+  render() {
+    const { item, handleChange, handleSubmit, editItem } = this.props;
+    return (
+      <div className="card card-body my-3">
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div
+                className={
+                  editItem
+                    ? "input-group-text bg-success text-white"
+                    : "input-group-text bg-primary text-white"
+                }
+              >
+                <i className="fas fa-book"></i>
+              </div>
             </div>
-        )
-    }
+            <input
+              type="text"
+              className="form-control "
+              placeholder="Add to-do item"
+              value={item}
+              onChange={handleChange}
+            />
+          </div>
+          <button
+            type="submit"
+            className={
+              editItem
+                ? "btn btn-block btn-success mt-3"
+                : "btn btn-block btn-primary mt-3"
+            }
+          >
+            {editItem ? "edit item" : "add item"}
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class TodoItem extends Component {
-    render() {
-        return (
-            <div>
-                Hello from Todo Item
-            </div>
-        )
-    }
+  render() {
+    const { title, handleDelete, handleEdit } = this.props;
+    return (
+      <li className="list-group-item text-capitalize d-flex justify-content-between my-3">
+        <h5>{title}</h5>
+        <div className="todo-icon">
+          <span className="mx-2 text-success" onClick={handleEdit}>
+            <i className="fas fa-pen"></i>
+          </span>
+          <span className="mx-2 text-danger" onClick={handleDelete}>
+            <i className="fas fa-trash"></i>
+          </span>
+        </div>
+      </li>
+    );
+  }
 }
